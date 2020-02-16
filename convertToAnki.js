19c21447ffc1a08ed53c0877b5216908579d7847
Timo Horstschaefer <mail@timo-horstschaefer.de>
2020-02-16 13:15:05 +0100

    add images

--- convertToAnki.js
+++ convertToAnki.js
@@ -1,7 +1,10 @@
 const { parse } = require('fast-xml-parser');
 const fs = require('fs');
 
-const xml = fs.readFileSync(process.argv[2], 'utf8');
+const xmlFile = process.argv[2];
+const imagePath = process.argv[3];
+
+const xml = fs.readFileSync(xmlFile, 'utf8');
 const data = parse(xml, { ignoreAttributes: false, attributeNamePrefix: '' });
 
 const tags = {
@@ -16,16 +19,22 @@ const tags = {
     90: 'Kommunikation' 
 }
 
-const getQuestion = ({ question, answer, id, chapter }) => ({
-    id,
-    question,
-    answer1: answer[0].text,
-    answer2: answer[1].text,
-    answer3: answer[2].text,
-    answer4: answer[3].text,
-    solution: answer.findIndex(v => v.isCorrect === 'TRUE') + 1,
-    tags: `${chapter} ${tags[chapter]}`
-});
+
+const getQuestion = ({ question, answer, id, chapter }) => {
+    const image = `p${id}.png`;
+    const hasImage = fs.existsSync(`${imagePath}/${image}`);
+    return ({
+        id,
+        question,
+        image: hasImage ? `<img src="${image}">` : '',
+        answer1: answer[0].text,
+        answer2: answer[1].text,
+        answer3: answer[2].text,
+        answer4: answer[3].text,
+        solution: answer.findIndex(v => v.isCorrect === 'TRUE') + 1,
+        tags: `${chapter} ${tags[chapter]}`
+    });
+}
 const questions = data.qdata.problem.map(getQuestion);
 
 Object.keys(tags).forEach(chapter => {
